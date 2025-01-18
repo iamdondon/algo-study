162d0e5c3d8003d14f764c2a5946aafd56f3ac9e
kdw <kimdon17@naver.com>
2025-01-18 21:49:13 +0900

    - 프로그래머스 lv1 - 10번 / 공원

--- solution/programmers/level1/Park.java
+++ solution/programmers/level1/Park.java
@@ -1,29 +1,43 @@
 package solution.programmers.level1;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Park {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{2,5,3}, new String[][]{
+                {"A", "A", "-1", "B", "B", "B", "B", "-1"},
+                {"A", "A", "-1", "B", "B", "B", "B", "-1"},
+        {"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"},
+            {"D", "D", "-1", "-1", "-1", "-1", "E", "-1"},
+                {"D", "D", "-1", "-1", "-1", "-1", "-1", "F"},
+                    {"D", "D", "-1", "-1", "-1", "-1", "E", "-1"}
+        }));
     }
 
-    private int[] dx = new int[]{0, 0, 1, -1};
-    private int[] dy = new int[]{1, -1, 0, 0};
-
     public static int solution(int[] mats, String[][] park) {
-        int answer = 0;
+        int answer = -1;
         Arrays.sort(mats);
 
-        for(int i = mats.length-1; i>0; i--){
+        for(int i = mats.length-1; i>=0; i--){
             int matLen = mats[i];
 
-            for(int x=0; x<park.length; x++){
+            for(int x=0; x<park.length-i; x++){
                 for(int y=0; y<park[0].length; y++){
-                    if(!park[x][y].equals("-1")) continue;
-
-                    dfs(x, y, mats, matLen);
+                    boolean chk = true;
+                    if(park[x][y].equals("-1")){
+                        for(int dx=0; dx<matLen; dx++){
+                            for (int dy=0; dy<matLen; dy++){
+                                if(x+dx >= park.length || y+dy >= park[0].length || !park[x+dx][y+dy].equals("-1")) {
+                                    chk = false;
+                                    break;
+                                }
+                            }
+                            if(!chk) break;
+                        }
+                        if(chk) return matLen;
+                    }
                 }
             }
         }
@@ -31,7 +45,5 @@ public class Park {
         return answer;
     }
 
-    private static void dfs(int x, int y, int[] mats, int matLen) {
 
-    }
 }
